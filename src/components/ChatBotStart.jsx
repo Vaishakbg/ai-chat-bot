92e3007ce58f9ee4ceb0b58f755cd0a71fc25748
Vaishakbg <vaishakbg@gmail.com>
2025-03-07 18:21:15 +0530

    Creating and Customizing Start Page

--- src/components/ChatBotStart.jsx
+++ src/components/ChatBotStart.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React from 'react';
+import './ChatBotStart.css';
 
 const ChatBotStart = () => {
   return (
-    <div>ChatBotStart</div>
+    <div className='start-page'>
+        <button className='start-page-btn'>Chat AI</button>
+    </div>
   )
 }
 
